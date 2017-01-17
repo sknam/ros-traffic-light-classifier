7da9b71db89ab9f65f1854d485cbf6731d5aae59
Eric Gonzalez <eric@plasticgnome.com>
2017-01-17 10:40:53 -0800

    Tensorboard Callback

--- model/train.py
+++ model/train.py
@@ -1,4 +1,5 @@
 from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import TensorBoard
 import model
 
 batch_size = 64
@@ -9,6 +10,9 @@ datagen = ImageDataGenerator(width_shift_range=.2, height_shift_range=.2, shear_
                              fill_mode='nearest', rescale=1. / 255)
 image_data_gen = datagen.flow_from_directory('images', target_size=(64, 64), classes=['green', 'red', 'unknown'],
                                              batch_size=batch_size)
-model.fit_generator(image_data_gen, nb_epoch=nb_epoch, samples_per_epoch=image_data_gen.nb_sample)
+
+tensorboard_callback = TensorBoard(log_dir='/tmp/tf-lights', histogram_freq=1)
+
+model.fit_generator(image_data_gen, nb_epoch=nb_epoch, samples_per_epoch=image_data_gen.nb_sample, callbacks=[tensorboard_callback])
 
 model.save('light_classifier_model.h5')
